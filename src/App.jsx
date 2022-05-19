3aa65bf0927c923e077a0c8c7e92516878b59c3f
Ridoy Hossain <findridoy@gmail.com>
2022-05-19 14:10:57 +0600

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function () {
+    return (
+        <div class="h-24 bg-gray-400">Header</div>
+    )
+}
